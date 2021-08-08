a878dc95c6fa8951e5cf43b2c6dab81aa766522f
Eugene Toropov <eugene.toropov@gmail.com>
2021-08-08 16:01:27 +0300

    :fire: leetcode submission codereview for my subscriber

---
+++ cmd/twosum/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"log"
+)
+
+func main() {
+	if err := run(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func run() error {
+	nums := []int{2, 7, 11, 15}
+	target := 9
+	fmt.Printf("Nums before: %#v\n", nums)
+	res := twoSum(nums, target)
+	fmt.Printf("Nums after: %#v\n", nums)
+	fmt.Printf("Result: %#v\n", res)
+
+	return nil
+}
+
+func twoSum(nums []int, target int) []int {
+	for i := 0; i < len(nums)-1; i++ {
+		for j := i + 1; j < len(nums); j++ {
+			if nums[i]+nums[j] == target {
+				return []int{i, j}
+			}
+		}
+	}
+	return nil
+}
