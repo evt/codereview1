e2a3e01067a1643b0576c7925a02edea526477df
Dimas Volkov <dimas.volkov@gmail.com>
2021-08-09 13:27:30 +0500

    two funcs test and bench

--- cmd/twosum/main_test.go
+++ cmd/twosum/main_test.go
@@ -2,38 +2,67 @@ package main
 
 import (
 	"fmt"
+	"reflect"
 	"testing"
-
-	"github.com/stretchr/testify/assert"
 )
 
-func Test_twoSum(t *testing.T) {
-	cases := []struct {
-		nums   []int
-		target int
-		want   []int
-	}{
-		{
-			nums:   []int{1, 2, 3},
-			target: 3,
-			want:   []int{0, 1},
-		},
-		{
-			nums:   []int{3, 2, 4},
-			target: 6,
-			want:   []int{1, 2},
-		},
-		{
-			nums:   []int{3, 3},
-			target: 6,
-			want:   []int{0, 1},
-		},
-	}
+var cases = []struct {
+	data     []int
+	target   int
+	expected []int
+}{
+	{
+		[]int{1, 2, 3},
+		3,
+		[]int{0, 1},
+	},
+	{
+		[]int{2, 3, 4, 5},
+		8,
+		[]int{1, 3},
+	},
+	{
+		[]int{3, 3},
+		6,
+		[]int{0, 1},
+	},
+	{
+		[]int{3, 3},
+		8,
+		nil,
+	},
+}
 
+func Test_doubleSumCycle(t *testing.T) {
 	for _, cs := range cases {
-		t.Run(fmt.Sprintf("twoSum(%#v, %d)", cs.nums, cs.target), func(t *testing.T) {
-			got := twoSum(cs.nums, cs.target)
-			assert.Equal(t, got, cs.want)
+		t.Run(fmt.Sprintf("Case %v, target %v", cs.data, cs.target), func(t *testing.T) {
+			result := doubleSumCycle(cs.data, cs.target)
+			if !reflect.DeepEqual(result, cs.expected) {
+				t.Error("unexpected result")
+			}
 		})
+	}
+}
+
+func Test_doubleSumMap(t *testing.T) {
+	for _, cs := range cases {
+		t.Run(fmt.Sprintf("Case %v, target %v", cs.data, cs.target), func(t *testing.T) {
+			result := doubleSumMap(cs.data, cs.target)
+			if !reflect.DeepEqual(result, cs.expected) {
+				t.Error("unexpected result")
+			}
+		})
+	}
+}
+
+func Benchmark_doubleSumCycle(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		doubleSumCycle([]int{2, 4, 5, 7, 11, 3, 6, 13, 10, 32, 23, 22, 44, 33, 1, 14, 42, 17, 26, 80}, 102)
+	}
+}
+
+func Benchmark_doubleSumMap(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		doubleSumMap([]int{2, 4, 5, 7, 11, 3, 6, 13, 10, 32, 23, 22, 44, 33, 1, 14, 42, 17, 26, 80}, 102)
 	}
 }
